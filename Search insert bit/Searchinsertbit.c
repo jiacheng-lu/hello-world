75d8b4bc6eb061797703072d71c0959d7cbc51bf
jiacheng-lu <63840399+jiacheng-lu@users.noreply.github.com>
2020-05-28 01:05:28 +0800

    Update Searchinsertbit.c

--- Search insert bit/Searchinsertbit.c
+++ Search insert bit/Searchinsertbit.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 int searchInsert(int* nums, int numsSize, int target){
 int  a=0,b=numsSize-1,mid;         //定义整型变量a,b 并给a赋初值0，b取nums的最大值
 while(a<=b){                       //当二分法最左边的端点小于等于右边端点时
@@ -13,12 +14,11 @@ return target<nums[mid]?mid:mid+1;  //当a>b时，如果目标值小于中位值
 }
 int main(){
 
-	int nums[100] = "1356";
+	char nums[] = "1356";
 
         int target = 5 ;
-
-	printf("%d\n",searchInsert(nums, target));
-
-	return 0;
+        printf("strlen = %d\n, strlen(nums));
+	
+        return 0;
 
 }
